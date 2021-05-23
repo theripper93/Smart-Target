aa4380dff1d1496685d7ebeb27bcdff6a8af936f
p4535992 <tentimarco0@gmail.com>
2021-05-23 13:50:23 +0200

    Update readme.md add some feature and settings

--- scripts/main.js
+++ scripts/main.js
@@ -1,125 +1,198 @@
+/**
+ * This is your TypeScript entry file for Foundry VTT.
+ * Register custom settings, sheets, and constants using the Foundry API.
+ * Change this heading to be more descriptive to your module, or remove it.
+ * Author: [your name]
+ * Content License: [copyright and-or license] If using an existing system
+ * 					you may want to put a (link to a) license or copyright
+ * 					notice here (e.g. the OGL).
+ * Software License: [your license] Put your desired license here, which
+ * 					 determines how others may use and modify your module
+ */
+
+const MODULE_NAME = 'smarttarget';
+
+// Import JavaScript modules
+
+// Import TypeScript modules
+
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
 Hooks.on("init", () => {
 
-    game.settings.register('smarttarget', 'altTarget', {
-		name: 'Target with Alt-Click',
-		hint: 'Target tokens by pressing Alt+Click, add Shift to target multiple',
-		scope: 'client',
-		config: true,
-		default: true,
-		type: Boolean,
-	});
+  game.settings.register("smarttarget", 'altTarget', {
+  name: game.i18n.localize("smarttarget.settings.altTarget.name"),
+  hint: game.i18n.localize("smarttarget.settings.altTarget.hint"),
+  scope: 'client',
+  config: true,
+  default: true,
+  type: Boolean,
+});
 
-    game.settings.register('smarttarget', 'portraitPips', {
-		name: 'Show portraits instead of colors as player target indicators',
-		hint: 'Uses avatar for GM, defaults to Token for players if no avatar is found for the assigned actor (requires refresh)',
-		scope: 'world',
-		config: true,
-		default: false,
-		type: Boolean,
-        onChange: settings => window.location.reload()
-	});
+  game.settings.register("smarttarget", 'portraitPips', {
+  name: game.i18n.localize("smarttarget.settings.portraitPips.name"),
+  hint: game.i18n.localize("smarttarget.settings.portraitPips.hint"),
+  scope: 'world',
+  config: true,
+  default: false,
+  type: Boolean,
+      onChange: settings => window.location.reload()
+});
 
-    game.settings.register("smarttarget", "pipImgScale", {
-      name: "Target Icon Image Scale",
-      hint: "Set the scale for the image used by the target icons (default: 1)",
+  game.settings.register("smarttarget", "pipImgScale", {
+    name: game.i18n.localize("smarttarget.settings.pipImgScale.name"),
+    hint: game.i18n.localize("smarttarget.settings.pipImgScale.hint"),
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0.05,
+      max: 10,
+      step: 0.05,
+    },
+    default: 1,
+  });
+
+  game.settings.register("smarttarget", "pipOffsetManualY", {
+      name: game.i18n.localize("smarttarget.settings.pipOffsetManualY.name"),
+      hint: game.i18n.localize("smarttarget.settings.pipOffsetManualY.hint"),
       scope: "world",
       config: true,
       type: Number,
       range: {
-        min: 0.05,
-        max: 10,
-        step: 0.05,
-      },
-      default: 1,
-    });
-
-    game.settings.register("smarttarget", "pipOffsetManualY", {
-        name: "Target Image Y Offset",
-        hint: "Add a flat offset to the image in pixels (default: 0)",
-        scope: "world",
-        config: true,
-        type: Number,
-        range: {
-          min: 0,
-          max: 100,
-          step: 0.05,
-        },
-        default: 0,
-      });
-
-      game.settings.register("smarttarget", "pipOffsetManualX", {
-        name: "Target Image X Offset",
-        hint: "Add a flat offset to the image in pixels (default: 0)",
-        scope: "world",
-        config: true,
-        type: Number,
-        range: {
-          min: 0,
-          max: 100,
-          step: 0.05,
-        },
-        default: 0,
-      });
-
-    game.settings.register("smarttarget", "pipScale", {
-      name: "Target Icon Size",
-      hint: "Set the size for the target icon in pixels (default: 12)",
-      scope: "world",
-      config: true,
-      type: Number,
-      range: {
-        min: 0.05,
+        min: 0,
         max: 100,
         step: 0.05,
       },
-      default: 12,
+      default: 0,
     });
 
-    game.settings.register("smarttarget", "pipOffset", {
-      name: "Target Icon Offset",
-      hint: "Set the distance between icons in pixels (default: 16)",
+    game.settings.register("smarttarget", "pipOffsetManualX", {
+      name: game.i18n.localize("smarttarget.settings.pipOffsetManualX.name"),
+      hint: game.i18n.localize("smarttarget.settings.pipOffsetManualX.hint"),
       scope: "world",
       config: true,
       type: Number,
       range: {
-        min: 0.05,
+        min: 0,
         max: 100,
         step: 0.05,
       },
-      default: 16,
+      default: 0,
     });
 
-    game.settings.register("smarttarget", "borderThicc", {
-      name: "Border Thickness",
-      hint: "Set the thickness of the border in pixels (default: 2)",
-      scope: "world",
-      config: true,
-      type: Number,
-      range: {
-        min: 2,
-        max: 10,
-        step: 1,
-      },
-      default: 2,
-    });
+  game.settings.register("smarttarget", "pipScale", {
+    name: game.i18n.localize("smarttarget.settings.pipScale.name"),
+    hint: game.i18n.localize("smarttarget.settings.pipScale.hint"),
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0.05,
+      max: 100,
+      step: 0.05,
+    },
+    default: 12,
+  });
 
-    game.settings.register('smarttarget', 'crossairSpread', {
-      name: 'Bring Targeting Arrows Closer Together',
-      hint: 'Bring the targeting arrows closer together so that they are inside the token frame',
-      scope: 'client',
-      config: true,
-      default: false,
-      type: Boolean,
-    });
+  game.settings.register("smarttarget", "pipOffset", {
+    name: game.i18n.localize("smarttarget.settings.pipOffset.name"),
+    hint: game.i18n.localize("smarttarget.settings.pipOffset.hint"),
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0.05,
+      max: 100,
+      step: 0.05,
+    },
+    default: 16,
+  });
 
-    game.settings.register("smarttarget", "crossairColor", {
-      name: "Targeting Arrows Color",
-      hint: "Hex color for the targeting arrows (default: #ff9829)",
-      scope: "client",
-      config: true,
-      type: String,
-      default: "#ff9829",
-    });
+  game.settings.register("smarttarget", "borderThicc", {
+    name: game.i18n.localize("smarttarget.settings.borderThicc.name"),
+    hint: game.i18n.localize("smarttarget.settings.borderThicc.hint"),
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 2,
+      max: 10,
+      step: 1,
+    },
+    default: 2,
+  });
+
+  game.settings.register("smarttarget", "crossairSpread", {
+    name: game.i18n.localize("smarttarget.settings.crossairSpread.name"),
+    hint: game.i18n.localize("smarttarget.settings.crossairSpread.hint"),
+    scope: 'client',
+    config: true,
+    default: false,
+    type: Boolean,
+  });
+
+  game.settings.register("smarttarget", "crossairColor", {
+    name: game.i18n.localize("smarttarget.settings.crossairColor.name"),
+    hint: game.i18n.localize("smarttarget.settings.crossairColor.hint"),
+    scope: "client",
+    config: true,
+    type: String,
+    // MOVE TO READY HOOK ???
+    //default: colorStringToHex(game.user['color'])
+    default: "#ff9829"
+  });
+
+  game.settings.register("smarttarget",'target-indicator',{
+    name: game.i18n.localize("smarttarget.settings.target-indicator.name"),
+    hint: game.i18n.localize("smarttarget.settings.target-indicator.hint"),
+    scope: "player",
+    config: true,
+    default: "0",
+    type: String,
+    choices: {
+        "0" : game.i18n.localize("smarttarget.settings.target-indicator-choices-0"),
+        "1" : game.i18n.localize("smarttarget.settings.target-indicator-choices-1"),
+        "2" : game.i18n.localize("smarttarget.settings.target-indicator-choices-2"),
+        "3" : game.i18n.localize("smarttarget.settings.target-indicator-choices-3"),
+        "4" : game.i18n.localize("smarttarget.settings.target-indicator-choices-4"),
+        "5" : game.i18n.localize("smarttarget.settings.target-indicator-choices-5"),
+    }
+  });
+
+  game.settings.register("smarttarget",'use-player-color', {
+    name : game.i18n.localize("smarttarget.settings.use-player-color.name"),
+    hint : game.i18n.localize("smarttarget.settings.use-player-color.hint"),
+    scope: "player",
+    config: true,
+    default: true,
+    type: Boolean
+  });
+
     
-    if(game.settings.get('smarttarget', 'portraitPips'))libWrapper.register("smarttarget","Token.prototype._refreshTarget", _refreshTarget, "OVERRIDE")
+  //if(game.settings.get("smarttarget", "portraitPips")){
+    libWrapper.register("smarttarget","Token.prototype._refreshTarget", _refreshTarget, "OVERRIDE")
+  //}
+
+  Hooks.on("getSceneControlButtons",getSceneControlButtonsHandler);
 })
+
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once('setup', function () {
+	// Do anything after initialization but before ready
+
+});
+
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once('ready', () => {
+	// Do anything once the module is ready
+  
+});
+
+// Add any additional hooks if necessary
+
