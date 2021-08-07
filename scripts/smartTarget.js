cc86f5ef031bde44f6427ee4ceaa709cb5eafacd
theripper93 <tsplab@gmail.com>
2021-08-08 00:57:08 +0200

    a

--- scripts/smartTarget.js
+++ scripts/smartTarget.js
@@ -142,15 +142,17 @@ class SmartTarget {
 
   // Draw custom crosshair and pips
   static async _refreshTarget() {
+
+    this.target.clear();
+    if (!this.targeted.size) return;
+
     if(game.user.isGM) {
       let flag
       const gmTexSetting = game.settings.get(SMARTTARGET_MODULE_NAME, "useTokenGm")
       if(gmTexSetting == 1) flag = _token?.actor?.data.img || _token?.data.img
       if(gmTexSetting == 2) flag = _token?.data.img || _token?.actor?.data.img 
-      flag && this.document.setFlag(SMARTTARGET_MODULE_NAME,"gmtargetimg",flag)
+      flag && flag != this.document.getFlag(SMARTTARGET_MODULE_NAME,"gmtargetimg") && this.document.setFlag(SMARTTARGET_MODULE_NAME,"gmtargetimg",flag)
     }
-    this.target.clear();
-    if (!this.targeted.size) return;
 
     // Determine whether the current user has target and any other users
     const [others, user] = Array.from(this.targeted).partition(
